d2f0cf2784688c2a88af7c524ac0151fd18beeb7
Clinton <clinton.almeida10@gmail.com>
2023-08-21 20:25:54 -0300

    Inluindo metodos na classe tarefas_model

--- tarefas/lib/models/tarefas_model.dart
+++ tarefas/lib/models/tarefas_model.dart
@@ -10,5 +10,33 @@ class Tarefas {
   double? progrecao;
   PadraoRepeticao? padraoRepeticao;
 
-  Tarefas({this.id, this.nomeTarefa, this.estado = true, this.nota});
+  Tarefas(int id, String nomeTarefa, bool estado) {
+    this.id = id;
+    this.nomeTarefa = nomeTarefa;
+    this.estado = true;
+  }
+
+  Map<String, dynamic> toMap({bool excludeId = false}) {
+    var map = <String, dynamic>{
+      'nomeTarefa': nomeTarefa,
+      'estado': estado,
+    };
+
+    if (!excludeId) {
+      map['id'] = id;
+    }
+    return map;
+  }
+
+  Tarefas.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    nomeTarefa = map['nomeTarefa'];
+    estado = map['estado'];
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Tarefa => (id: $id, nomeTarefa: $nomeTarefa, estado: $estado)';
+  }
 }
