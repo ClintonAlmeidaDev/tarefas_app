3b46856de35d857751d1c7265e48002180ddd653
Clinton <clinton.almeida10@gmail.com>
2023-08-21 19:33:59 -0300

    Subindo projeto

---
+++ tarefas/lib/models/enums_model.dart
@@ -0,0 +1,9 @@
+enum Prioridade { alta, media, baixa }
+
+enum PadraoRepeticao {
+  diariamente,
+  semanalmente,
+  mensalmente,
+  anualmente,
+  nunca
+}
---
+++ tarefas/lib/models/tarefas_model.dart
@@ -0,0 +1,12 @@
+import 'enums_model.dart';
+
+class tarefas {
+  int? id;
+  String? descricao;
+  bool? estado;
+  Prioridade? prioridade;
+  List<String>? tags;
+  String? nota;
+  double? progracao;
+  PadraoRepeticao? padraoRepeticao;
+}
